ec5bd90850881ff440340645f647b3fd8d141cfc
riyajha2305 <riya.jha2305@gmail.com>
2022-01-29 14:09:45 +0530

    Final commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:'acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~'
+}
