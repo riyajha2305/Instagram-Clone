9e19bf8d5135f649fee6adee88bc7fd1df6c7dfa
Harshit <2020ume0200@iitjammu.ac.in>
2022-02-07 03:38:40 +0530

    Added the google signup/signin

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,6 @@
-module.exports={
-    MONGOURI:"mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:'acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~'
-}
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~",
+  GOOGLE_CLIENT_ID: "Enter your google client id",
+};
