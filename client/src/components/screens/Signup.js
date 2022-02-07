5eec9a3d86a8ab14799c14c378fedae2d25e2605
RiyaJha <78296094+riyajha2305@users.noreply.github.com>
2022-02-07 19:24:12 +0530

    Revert "Added the google signup/signin"

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,249 +1,161 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import M from "materialize-css";
-import GoogleLogin from "react-google-login";
-import FacebookLogin from "react-facebook-login";
-
-const SignIn = () => {
-  const history = useHistory();
-  const [name, setName] = useState("");
-  const [password, setPasword] = useState("");
-  const [email, setEmail] = useState("");
-  const [image, setImage] = useState("");
-  const [url, setUrl] = useState(undefined);
-  useEffect(() => {
-    if (url) {
-      uploadFields();
-    }
-  }, [url]);
-  const uploadPic = () => {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "insta-clone");
-    data.append("cloud_name", "dqjgjdewi");
-    fetch("https://api.cloudinary.com/v1_1/dqjgjdewi/image/upload", {
-      method: "post",
-      body: data,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setUrl(data.url);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const uploadFields = () => {
-    if (
-      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-      )
-    ) {
-      M.toast({ html: "invalid email", classes: "#c62828 red darken-3" });
-      return;
-    }
-    fetch("/signup", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        password,
-        email,
-        pic: url,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.error) {
-          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-        } else {
-          M.toast({ html: data.message, classes: "#43a047 green darken-1" });
-          history.push("/signin");
+import React,{useState,useEffect} from 'react'
+import {Link,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
+const SignIn  = ()=>{
+    const history = useHistory()
+    const [name,setName] = useState("")
+    const [password,setPasword] = useState("")
+    const [email,setEmail] = useState("")
+    const [image,setImage] = useState("")
+    const [url,setUrl] = useState(undefined)
+    useEffect(()=>{
+        if(url){
+            uploadFields()
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const PostData = () => {
-    if (image) {
-      uploadPic();
-    } else {
-      uploadFields();
+    },[url])
+    const uploadPic = ()=>{
+        const data = new FormData()
+        data.append("file",image)
+        data.append("upload_preset","insta-clone")
+        data.append("cloud_name","dqjgjdewi")
+        fetch("https://api.cloudinary.com/v1_1/dqjgjdewi/image/upload",{
+            method:"post",
+            body:data
+        })
+        .then(res=>res.json())
+        .then(data=>{
+           setUrl(data.url)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
     }
-  };
-
-  const GoogleSignupFaliure = () => {
-    M.toast({
-      html: <h1>Unable to Sign up</h1>,
-      classes: "#c62828 red darken-3",
-    });
-  };
-
-  const GoogleSignupSuccess = (res) => {
-    fetch("/googleSignup", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        tokenId: res.tokenId,
-      }),
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        if (data.error) {
-          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-        } else {
-          M.toast({
-            html: "Sign up successfully",
-            classes: "#43a047 green darken-1",
-          });
-          history.push("/signin");
+    const uploadFields = ()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
+            return
         }
-      });
-  };
-
-  const facebookSignup = (res) => {
-    fetch("/facebookSignup", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userId: res.userID,
-        accessToken: res.accessToken,
-      }),
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        if (data.error) {
-          M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-        } else {
-          M.toast({
-            html: "Sign up successfully",
-            classes: "#43a047 green darken-1",
-          });
-          history.push("/signin");
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email,
+                pic:url
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+           if(data.error){
+              M.toast({html: data.error,classes:"#c62828 red darken-3"})
+           }
+           else{
+               M.toast({html:data.message,classes:"#43a047 green darken-1"})
+               history.push('/signin')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const PostData = ()=>{
+        if(image){
+            uploadPic()
+        }else{
+            uploadFields()
         }
-      })
-      .catch((e) => console.log(e));
-  };
+       
+    }
 
-  return (
-    <div className="mycard">
-      <div className="card auth-card input-field">
-        <h2>Instagram</h2>
-        <h6 className="welcometext">
-          Sign up to see photos and videos from your friends.
-        </h6>
-        <button className="btn" onClick={() => PostData()}>
-          Log in with facebook
-        </button>
-        <h6 className="orline">--------------- OR ----------------</h6>
-
-        <form className="formstyle">
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </form>
-        <form className="formstyle">
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </form>
-        <form className="formstyle">
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPasword(e.target.value)}
-          />
-        </form>
-        <div className="file-field input-field">
-          <div className="btnuploadphoto">
-            <span>Upload picture</span>
-            <input type="file" onChange={(e) => setImage(e.target.files[0])} />
-          </div>
-          <div className="file-path-wrapper">
-            <input
-              className="file-path validate"
-              type="text"
-              placeholder="Choose a picture from gallery"
-            />
-          </div>
+   return (
+      <div className="mycard">
+          <div className="card auth-card input-field">
+            <h2>Instagram</h2>
+            <h6 className="welcometext">
+            Sign up to see photos and videos from your friends.
+            </h6>
+            <button className="btn"
+            onClick={()=>PostData()}
+            >
+                Log in with facebook
+            </button>
+            <h6 className="orline">--------------- OR ----------------</h6>
+            
+            <form className='formstyle'>
+                <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+            </form>
+            <form className='formstyle'>
+                <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                />
+            </form>
+            <form className='formstyle'>
+                <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e)=>setPasword(e.target.value)}
+                />
+            </form>
+            <div className="file-field input-field">
+            <div className="btnuploadphoto">
+                <span>Upload picture</span>
+                <input type="file" onChange={(e)=>setImage(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" placeholder="Choose a picture from gallery"/>
+            </div>
+            </div>
+            <button className="btn"
+            onClick={()=>PostData()}
+            >
+                Sign UP
+            </button>
+            <div className="termncond">
+                <h6 >By signing up, you agree to our Terms , Data Policy and Cookies Policy .</h6>
+            </div>
         </div>
-        <FacebookLogin
-          appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID}
-          callback={facebookSignup}
-          fields="name,email,picture"
-        />
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Sign up with google"
-          cookiePolicy={"single_host_origin"}
-          onSuccess={GoogleSignupSuccess}
-          onFailure={GoogleSignupFaliure}
-        />
-        <button className="btn" onClick={() => PostData()}>
-          Sign UP
-        </button>
-        <div className="termncond">
-          <h6>
-            By signing up, you agree to our Terms , Data Policy and Cookies
-            Policy .
-          </h6>
-        </div>
-      </div>
-
-      <div className="card2">
+        <div className="card2">
         <div className="rowcard2">
-          <div className="columncard2">
-            <h6>
-              <Link to="/signin">Already have an account ?</Link>
-            </h6>
-          </div>
-          <div className="columncard2">
-            <h6>
-              <Link to="/signin">
-                <h6 className="signbtn">Log In</h6>
-              </Link>
-            </h6>
-          </div>
+                <div class="columncard2">
+                    <h6>
+                    <Link to="/signin">Already have an account ?</Link>
+                    </h6>
+                </div>
+                <div class="columncard2">
+                    <h6>
+                    <Link to="/signin"><h6 className="signbtn">Log In</h6></Link>
+                    </h6>
+                </div>
+            </div>
+        </div>
+        <div className="getapp">
+            <h6>Get the app</h6>
+            <div className="rowgetapp">
+                <div class="columngetapp">
+                    <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
+                     alt="AppStore" width="150" 
+                     height="40"/>
+                </div>
+                <div class="columngetapp">
+                    <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png" 
+                    alt="GooglePlay"  width="150" 
+                    height="40"/>
+                </div>
+            </div>
         </div>
       </div>
-      <div className="getapp">
-        <h6>Get the app</h6>
-        <div className="rowgetapp">
-          <div className="columngetapp">
-            <img
-              src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
-              alt="AppStore"
-              width="150"
-              height="40"
-            />
-          </div>
-          <div className="columngetapp">
-            <img
-              src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
-              alt="GooglePlay"
-              width="150"
-              height="40"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+   )
+}
 
-export default SignIn;
+
+export default SignIn
--- models/user.js
+++ models/user.js
@@ -1,28 +1,26 @@
-const mongoose = require("mongoose");
-const { ObjectId } = mongoose.Schema.Types;
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    // required:true //Removed Because google login doesn't required password
-  },
-  resetToken: String,
-  expireToken: Date,
-  pic: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dqjgjdewi/image/upload/v1642069378/default_pnnsh5.png",
-  },
-  followers: [{ type: ObjectId, ref: "User" }],
-  following: [{ type: ObjectId, ref: "User" }],
-  facebookId: { type: String, unique: true },
-});
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    resetToken:String,
+    expireToken:Date,
+    pic:{
+     type:String,
+     default:"https://res.cloudinary.com/dqjgjdewi/image/upload/v1642069378/default_pnnsh5.png"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
 
-mongoose.model("User", userSchema);
+mongoose.model("User",userSchema)
