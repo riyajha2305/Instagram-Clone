94c0d0225366be0800e1b13f6809f5d1e8878d08
Shruti Goyal <72005584+Shruti025@users.noreply.github.com>
2022-02-08 20:36:24 +0530

    Update UserProfile.js
    Resolving conflicts with main

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -1,7 +1,7 @@
 import React,{useEffect,useState,useContext} from 'react'
 import {UserContext} from '../../App'
 import {useParams} from 'react-router-dom'
-import ".Profile.css"
+import "./Profile.css"
 const Profile  = ()=>{
     const [userProfile,setProfile] = useState(null)
     
