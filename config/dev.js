69747eda3fe0d3c3642ac9f465d2239b6ef92f93
harshit-070 <98070994+harshit-070@users.noreply.github.com>
2022-02-10 11:37:29 +0530

    Delete the database key

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
-module.exports={
-    MONGOURI:"mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:'acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~'
-}
+module.exports = {
+  MONGOURI: "mongodb://localhost:27017/test",
+  JWT_SECRET: "acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~",
+};
