4e263b809bc49ddac5bca7c49452c38eabe738dd
Amirul Sekh <sekhemarul123@gmail.com>
2022-03-02 13:50:06 +0530

    aded show more commments feature

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../App";
 import { Link } from "react-router-dom";
+import SinglePost from "./SinglePost";
 import "./Home.css";
 const Home = () => {
   const [data, setData] = useState([]);
@@ -178,100 +179,17 @@ const Home = () => {
     <div className="home">
       {data.map((item) => {
         return (
-          <div className="card home-card" key={item._id}>
-            <div className="card-user-details">
-              <Link
-                to={
-                  item.postedBy._id !== state._id
-                    ? "/profile/" + item.postedBy._id
-                    : "/profile"
-                }
-              >
-                <img className="card-user-pic" src={item.postedBy.pic} />
-              </Link>
-              <h5>
-                <Link
-                  to={
-                    item.postedBy._id !== state._id
-                      ? "/profile/" + item.postedBy._id
-                      : "/profile"
-                  }
-                >
-                  {item.postedBy.name}
-                </Link>
-                {item.postedBy._id == state._id && (
-                  <span>
-                    {" "}
-                    <i
-                      className="material-icons delete-post"
-                      onClick={() => deletePost(item._id)}
-                    >
-                      delete
-                    </i>
-                  </span>
-                )}
-              </h5>
-            </div>
-            <div className="card-image">
-              <img src={item.photo} />
-            </div>
-            <div className="card-content">
-              <i className="material-icons favorite">favorite</i>
-              {item.likes.includes(state._id) ? (
-                <i
-                  className="material-icons"
-                  onClick={() => {
-                    unlikePost(item._id);
-                  }}
-                >
-                  thumb_down
-                </i>
-              ) : (
-                <i
-                  className="material-icons user-select-none"
-                  onClick={() => {
-                    likePost(item._id);
-                  }}
-                >
-                  thumb_up
-                </i>
-              )}
-
-              <h6 className="like-count">{item.likes.length} likes</h6>
-              <h6 className="post-title">{item.title}</h6>
-              <p className="post-body">{item.body}</p>
-              {item.comments.map((record) => {
-                return (
-                  <h6 key={record._id}>
-                    <span className="posted-by">{record.postedBy.name}</span>{" "}
-                    {record.text}
-                    {record.postedBy._id == state._id && (
-                      <i
-                        className="material-icons delete-comment"
-                        onClick={() => deleteComment(item._id, record._id)}
-                      >
-                        delete
-                      </i>
-                    )}
-                  </h6>
-                );
-              })}
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  makeComment(item._id);
-                }}
-              >
-                <input
-                  type="text"
-                  disabled={disableInput}
-                  value={comment}
-                  onChange={(event) => setComment(event.target.value)}
-                  placeholder="Add a comment..."
-                />
-              </form>
-            </div>
-          </div>
+          <SinglePost
+            item={item}
+            disableInput={disableInput}
+            setComment={setComment}
+            likePost={likePost}
+            comment={comment}
+            unlikePost={unlikePost}
+            makeComment={makeComment}
+            deletePost={deletePost}
+            deleteComment={deleteComment}
+          />
         );
       })}
     </div>
