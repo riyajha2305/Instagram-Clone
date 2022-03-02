6cbde80951d5f333d5b07c1347de0800799f4b37
Nimishavashistha <69503770+Nimishavashistha@users.noreply.github.com>
2022-03-02 21:50:03 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 module.exports={
-    // MONGOURI:"mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    MONGOURI:"mongodb://nimi_vash:123@cluster0-shard-00-00.amyso.mongodb.net:27017,cluster0-shard-00-01.amyso.mongodb.net:27017,cluster0-shard-00-02.amyso.mongodb.net:27017/instagram_clone?ssl=true&replicaSet=atlas-wtbq81-shard-0&authSource=admin&retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://Riya:test-123@cluster0.vlz7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     JWT_SECRET:'acda,jgcyaiwegfhwbclwhVCBwcECCWJEB1234R11!@#@~'
 }
